b3af404a095957b8c814d8b2fe7ea927fab89d81
ATX <thahn1230@g.skku.edu>
2023-07-03 20:20:16 +0900

    modified

--- frontend/src/component/SubBuildingComponent/analysisGrid.tsx
+++ frontend/src/component/SubBuildingComponent/analysisGrid.tsx
@@ -39,13 +39,43 @@ const TotalAnalysisGrid2 = (props: any) => {
       {selectedBuildingInfo && (
         <div className="table-container">
           <Grid data={selectedBuildingInfo}>
-            <GridColumn title="구분" field="component_type" width={"85%"} headerClassName="custom-header-cell"/>
-            <GridColumn title="콘크리트(m³)" field="concrete_volume" width={"115%"} headerClassName="custom-header-cell"/>
-            <GridColumn title="콘크리트(%)" field="concrete_percentage" headerClassName="custom-header-cell"/>
-            <GridColumn title="거푸집(㎡)" field="formwork_area" headerClassName="custom-header-cell"/>
-            <GridColumn title="거푸집(%)" field="formwork_percentage" headerClassName="custom-header-cell"/>
-            <GridColumn title="철근(Ton)" field="rebar_weight" headerClassName="custom-header-cell"/>
-            <GridColumn title="철근(%)" field="rebar_percentage" headerClassName="custom-header-cell"/>
+            <GridColumn
+              title="구분"
+              field="component_type"
+              width={"100%"}
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="콘크리트(m³)"
+              field="concrete_volume"
+              width={"115%"}
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="콘크리트(%)"
+              field="concrete_percentage"
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="거푸집(㎡)"
+              field="formwork_area"
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="거푸집(%)"
+              field="formwork_percentage"
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="철근(Ton)"
+              field="rebar_weight"
+              headerClassName="custom-header-cell"
+            />
+            <GridColumn
+              title="철근(%)"
+              field="rebar_percentage"
+              headerClassName="custom-header-cell"
+            />
           </Grid>
         </div>
       )}
--- frontend/src/component/SubBuildingComponent/subBuildingTotalAnalysisTable4.tsx
+++ frontend/src/component/SubBuildingComponent/subBuildingTotalAnalysisTable4.tsx
@@ -19,9 +19,7 @@ const SubBuildingAnalysisTable4 = (props: any) => {
   const [selectedSubBuildingInfo, setSelectedSubBuildingInfo] =
     useState<subBuildingTotalAnalysisTable2_interface[]>();
 
-    
   const [returnDiv, setReturnDiv] = useState(<div></div>);
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -54,58 +52,58 @@ const SubBuildingAnalysisTable4 = (props: any) => {
 
   useEffect(() => {
     if (
-        typeof selectedSubBUildingId === "number" &&
-        selectedSubBUildingId !== 0
-      ) {
-    setReturnDiv(  <div>
-        {selectedSubBuildingInfo && (
-          <div className="table-container">
-            <Grid data={selectedSubBuildingInfo}>
-              <GridColumn
-                title="구분"
-                field="component_type"
-                width={"85%"}
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="콘크리트(m³)"
-                field="concrete_volume"
-                width={"115%"}
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="콘크리트(%)"
-                field="concrete_percentage"
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="거푸집(㎡)"
-                field="formwork_area"
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="거푸집(%)"
-                field="formwork_percentage"
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="철근(Ton)"
-                field="rebar_weight"
-                headerClassName="custom-header-cell"
-              />
-              <GridColumn
-                title="철근(%)"
-                field="rebar_percentage"
-                headerClassName="custom-header-cell"
-              />
-            </Grid>
-          </div>
-        )}
-      </div>)
-      };
+      typeof selectedSubBUildingId === "number" &&
+      selectedSubBUildingId !== 0
+    ) {
+      setReturnDiv(
+        <div>
+          {selectedSubBuildingInfo && (
+            <div className="table-container">
+              <Grid data={selectedSubBuildingInfo}>
+                <GridColumn
+                  title="구분"
+                  field="component_type"
+                  width={"100%"}
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="콘크리트(m³)"
+                  field="concrete_volume"
+                  width={"115%"}
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="콘크리트(%)"
+                  field="concrete_percentage"
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="거푸집(㎡)"
+                  field="formwork_area"
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="거푸집(%)"
+                  field="formwork_percentage"
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="철근(Ton)"
+                  field="rebar_weight"
+                  headerClassName="custom-header-cell"
+                />
+                <GridColumn
+                  title="철근(%)"
+                  field="rebar_percentage"
+                  headerClassName="custom-header-cell"
+                />
+              </Grid>
+            </div>
+          )}
+        </div>
+      );
+    }
   }, [selectedSubBuildingInfo]);
-
-  
 
   return returnDiv;
 };
