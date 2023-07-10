64a48d7e061af345e0f80a0a7dc322693a2670eb
unknown <cksdyd8350@gmail.com>
2023-07-10 18:05:22 +0900

    added insight filter list

--- frontend/src/component/insightComponent/insightList.tsx
+++ frontend/src/component/insightComponent/insightList.tsx
@@ -1,12 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import axios from "axios";
 import urlPrefix from "./../../resource/URL_prefix.json";
 import {
   DropDownList,
   MultiSelect,
   MultiSelectChangeEvent,
+  MultiSelectTree,
+  MultiSelectTreeChangeEvent,
+  MultiSelectTreeExpandEvent,
+  getMultiSelectTreeValue,
 } from "@progress/kendo-react-dropdowns";
 import { Button } from "@progress/kendo-react-buttons";
+import { itemIndexStartsWith } from "@progress/kendo-react-dropdowns/dist/npm/common/utils";
+
+const dataItemKey = "id";
+const checkField = "checkField";
+const checkIndeterminateField = "checkIndeterminateField";
+const subItemsField = "";
+const expandField = "expanded";
+// const projectsTextField = "projectName";
+// const constructionCompanyTextField = "constructionCompany";
+
+const selectDropDownFields = {
+  dataItemKey,
+  checkField,
+  checkIndeterminateField,
+  expandField,
+  //subItemsField,
+};
 
 const InsightList = (props: any) => {
   const [insightList, setInsightList] = useState<string[]>([
@@ -24,14 +45,19 @@ const InsightList = (props: any) => {
   //actually selected value
   const [selectedInsightIndex, setSelectedInsightIndex] = useState(-1);
 
+  //constructionCompanyList's id is useless
   const [constructionCompanyList, setConstructionCompanyList] = useState<
-    string[]
-  >([]);
+    { constructionCompany: string; id: number }[]
+  >([{ constructionCompany: "All", id: 0 }]);
   const [selectedConstructionCompanyList, setSelectedConstructionCompanyList] =
-    useState<string[]>([]);
+    useState<{ constructionCompany: string; id: number }[]>([]);
 
-  const [projectList, setProjectList] = useState<string[]>([]);
-  const [selectedProjectList, setSelectedProjectList] = useState<string[]>([]);
+  const [projectList, setProjectList] = useState<
+    { projectName: string; id: number }[]
+  >([{ projectName: "All", id: 0 }]);
+  const [selectedProjectList, setSelectedProjectList] = useState<
+    { projectName: string; id: number }[]
+  >([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,16 +68,21 @@ const InsightList = (props: any) => {
         const data = JSON.parse(response.data);
 
         setProjectList(
-          ["All"].concat(
-            Array.from(new Set(data.map((item: any) => item.project_name)))
-          )
+          data.map((item: any) => {
+            return { projectName: item.project_name, id: item.id };
+          })
+        );
+
+        const uniqueConstructionCompanies = Array.from(
+          new Set(data.map((item: any) => item.construction_company))
         );
         setConstructionCompanyList(
-          ["All"].concat(
-            Array.from(
-              new Set(data.map((item: any) => item.construction_company))
-            )
-          )
+          uniqueConstructionCompanies.map((constructionCompany: any) => {
+            const item = data.find(
+              (item: any) => item.construction_company === constructionCompany
+            );
+            return { constructionCompany, id: item.id };
+          })
         );
       } catch (error) {
         console.error(error);
@@ -61,31 +92,40 @@ const InsightList = (props: any) => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    console.log(projectList);
+  }, [projectList]);
   const onSelectedInsightChange = (e: any) => {
     setSelectedInsightIndexInList(e.target.index);
     setSelectedInsightInList(e.value);
   };
 
-  useEffect(() => {
-    props.setSelectedInsightIndex(selectedInsightIndex);
-  }, [selectedInsightIndex]);
-
   const onButtonClick = () => {
     setSelectedInsightIndex(selectedInsightIndexInList);
   };
 
-  const onNewConstructionCompanySelection = (event: MultiSelectChangeEvent) => {
-    console.log(event);
-    if (event.value.includes("All"))
-     {
-        setSelectedConstructionCompanyList(constructionCompanyList);
-    } else {
-      setSelectedConstructionCompanyList([...event.value]);
-    }
+  const onNewConstructionCompanySelection = (
+    event: MultiSelectTreeChangeEvent
+  ) => {
+    // console.log(event)
+    setSelectedConstructionCompanyList(
+      getMultiSelectTreeValue(constructionCompanyList, {
+        ...selectDropDownFields,
+        ...event,
+        value: selectedConstructionCompanyList,
+      })
+    );
   };
 
-  const onNewProjectSelection = (event: MultiSelectChangeEvent) => {
-    setSelectedProjectList([...event.value]);
+  const onNewProjectSelection = (event: MultiSelectTreeChangeEvent) => {
+    // console.log(event)
+    setSelectedProjectList(
+      getMultiSelectTreeValue(projectList, {
+        ...selectDropDownFields,
+        ...event,
+        value: selectedProjectList,
+      })
+    );
   };
 
   return (
@@ -99,17 +139,29 @@ const InsightList = (props: any) => {
       />
       <Button onClick={onButtonClick}>Search</Button>
 
-      <MultiSelect
+      <MultiSelectTree
         style={{ width: "20%", margin: "10px" }}
         data={constructionCompanyList}
         value={selectedConstructionCompanyList}
         onChange={onNewConstructionCompanySelection}
+        textField="constructionCompany"
+        dataItemKey="id"
+        checkField={checkField}
+        checkIndeterminateField={checkIndeterminateField}
+        expandField={expandField}
       />
-      <MultiSelect
+
+      <MultiSelectTree
         style={{ width: "20%", margin: "10px" }}
         data={projectList}
         value={selectedProjectList}
         onChange={onNewProjectSelection}
+        textField="projectName"
+        dataItemKey="id"
+        checkField={checkField}
+        checkIndeterminateField={checkIndeterminateField}
+        expandField={expandField}
+        //onExpandChange={onProjectExpandChange}
       />
     </div>
   );
