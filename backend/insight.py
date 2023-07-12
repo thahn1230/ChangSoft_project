6d79904e4c6c423a038cef6f53375ad8be1265c4
ATX <thahn1230@g.skku.edu>
2023-07-12 11:00:10 +0900

    modified backend code

--- backend/insight.py
+++ backend/insight.py
@@ -17,9 +17,11 @@ import random
 import json
 import plotly
 import re
+import math
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 
+from typing import List
 from dbAccess import create_db_connection
 from plotly.subplots import make_subplots
 
@@ -155,6 +157,50 @@ def get_materials_data_by_company(construction_company, project_limit):
     return df
 
 
+def get_materials_data_by_project_ids_company(construction_company: str, project_ids: List[int]):
+    # Get project names for the construction company '우미건설'
+
+    for project_id in project_ids:
+        project_ids_str = ", ".join(str(project_id) for project_id in project_ids)
+    
+    query_projects = f"""
+    SELECT project_name
+    FROM project
+    WHERE construction_company = "{construction_company}"
+    AND id IN ({project_ids_str});
+"""
+    projects_df = pd.read_sql_query(query_projects, engine)
+    project_names = projects_df["project_name"].tolist()
+
+    # Iterate over project names and building names
+    results = []
+    for project_name in project_names:
+        query_buildings = f"""
+            SELECT building_name 
+            FROM building 
+            WHERE project_id = (SELECT id FROM project WHERE project_name = '{project_name}')
+        """
+        buildings_df = pd.read_sql_query(query_buildings, engine)
+        building_names = buildings_df["building_name"].tolist()
+
+        for building_name in building_names:
+            concrete, rebar, formwork = get_material_values(project_name, building_name)
+            results.append(
+                {
+                    "project_name": project_name,
+                    "building_name": building_name,
+                    "concrete": concrete,
+                    "rebar": rebar,
+                    "formwork": formwork,
+                }
+            )
+
+    # Create a DataFrame from the results
+    df = pd.DataFrame(results)
+    df["rebar_per_conc"] = df["rebar"] / df["concrete"]
+    return df
+
+
 # Function to generate random colors
 def random_color(n):
     return [
@@ -225,15 +271,26 @@ def extract_keywords(input_string):
 
 
 @router.get("/insight/1")
-def get_insight_1():
+def get_insight_1(project_ids_str):
     # 예제 1번
     # company_name의 모든 프로젝트에 있는 빌딩별, 콘크리트 M3당 철근량(ton) 값에 대한 분석
     # 그래프 1 : 빌딩별 콘크리트 M3당 철근량(ton) 값의 분포 히스토그램
     # 그래프 2 : 프로젝트별 콘크리트 M3당 철근량(ton) 값의 평균값 비교
-
+    project_ids = json.loads(project_ids_str)
+    
+    for project_id in project_ids:
+        query=f"""
+            SELECT * FROM project 
+            WHERE id = {project_id}
+        """
+        data_df = pd.read_sql(query, engine)
+        company_name = data_df["construction_company"].values[0]
+        break
+            
     # 계산 로직
-    company_name = "우미건설"
-    df = get_materials_data_by_company(company_name, 5)
+    df = get_materials_data_by_project_ids_company(company_name, project_ids)
+    
+    
     # 그래프1
     # make plotly histogram chart
     # Create a histogram
@@ -241,9 +298,9 @@ def get_insight_1():
 
     # Create a layout
     layout = go.Layout(
-        title_text=f"""{company_name}, 빌딩별 콘크리트 볼륨(m^3)당 철근량(ton)의 분포표""",
+        title_text=f"""{company_name}, 빌딩별 콘크리트 볼륨(㎥)당 철근량(ton)의 분포표""",
         title_font=dict(size=24, color="black"),
-        xaxis_title="콘크리트 볼륨(m^3)당 철근량(ton)",
+        xaxis_title="콘크리트 볼륨(㎥)당 철근량(ton)",
         yaxis_title="빈도(빌딩수)",
         bargap=0.1,  # gap between bars of adjacent location coordinates
     )
@@ -285,10 +342,10 @@ def get_insight_1():
 
     # Create a layout
     layout = go.Layout(
-        title=f"""{company_name}, 프로젝트별 빌딩의 콘크리트 볼륨(m^3)당 철근량(ton)의 평균값""",
+        title=f"""{company_name}, 프로젝트별 빌딩의 콘크리트 볼륨(㎥)당 철근량(ton)의 평균값""",
         title_font=dict(size=24, color="black"),
         xaxis=dict(title="프로젝트명"),
-        yaxis=dict(title="콘크리트 볼륨(m^3)당 철근량(ton)의 평균값"),
+        yaxis=dict(title="콘크리트 볼륨(㎥)당 철근량(ton)의 평균값"),
         autosize=False,
         width=1000,
         height=600,
@@ -325,17 +382,28 @@ def get_insight_1():
 
     # Convert the list to json
     figures_json = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+    
     return figures_json
 
 
 @router.get("/insight/2")
-def get_insight_2():
+def get_insight_2(project_ids_str):
     # 예제 2번
     # company_name의 모든 프로젝트에 대해, 프로젝트별 빌딩의 콘크리트 M3당 철근량(ton) 값의 분포 분석
     # 그래프 1 : 빌딩별 콘크리트 M3당 철근량(ton) 값의 Box plot 그래프
 
-    company_name = "계룡건설"
-    df = get_materials_data_by_company(company_name, 5)
+    project_ids = json.loads(project_ids_str)
+    
+    for project_id in project_ids:
+        query=f"""
+            SELECT * FROM project 
+            WHERE id = {project_id}
+        """
+        data_df = pd.read_sql(query, engine)
+        company_name = data_df["construction_company"].values[0]
+        break
+    
+    df = get_materials_data_by_project_ids_company(company_name, project_ids)
 
     # Let's say 'df' is your original DataFrame
     fig = px.box(df, x="project_name", y="rebar_per_conc")
@@ -343,10 +411,10 @@ def get_insight_2():
 
     # Customize the layout
     fig.update_layout(
-        title=f"{company_name}, 프로젝트별 소속빌딩의 콘크리트 M3당 철근량(ton) 값의 분포 분석",
+        title=f"{company_name}, 프로젝트별 소속빌딩의 콘크리트 ㎥당 철근량(ton) 값의 분포 분석",
         title_font=dict(size=20, color="black"),
         xaxis_title="프로젝트명",
-        yaxis_title="콘크리트 볼륨(m^3)당 철근량(ton)",
+        yaxis_title="콘크리트 볼륨(㎥)당 철근량(ton)",
         autosize=False,
         width=900,
         height=600,
@@ -362,7 +430,7 @@ def get_insight_2():
 
     fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    explanation = f"""이 그래프는 {company_name}의 각 프로젝트에서 빌딩별 콘크리트 볼륨(m^3) 당 철근량(ton)의 분포를 시각화한 것입니다. 
+    explanation = f"""이 그래프는 {company_name}의 각 프로젝트에서 빌딩별 콘크리트 볼륨(㎥) 당 철근량(ton)의 분포를 시각화한 것입니다. 
     x축은 각각의 프로젝트를, y축은 콘크리트 볼륨당 철근량을 나타냅니다.
     이 그래프의 형태는 박스플롯(Box Plot)이며, 박스플롯은 데이터의 중앙값, 사분위수, 이상치 등의 통계적 분포를 한 눈에 보기 쉽게 표현해주는 그래프입니다. 
     박스의 중앙선은 중앙값을, 박스의 상단과 하단은 각각 3사분위수(Q3)와 1사분위수(Q1)를 나타냅니다. 
@@ -376,54 +444,72 @@ def get_insight_2():
 
 
 @router.get("/insight/3")
-def get_insight_3():
+def get_insight_3(project_ids_str):
     # 예제 3번
     # company_name의 4개 프로젝트에 대해, 프로젝트별 빌딩의 콘크리트 종류별 사용비율 비교
     # 그래프 1 : 프로젝트별 콘크리트 종류별 사용비율 비교 그래프
 
-    # '우미건설'의 프로젝트 이름을 가져오는 쿼리
-    company_name = "우미건설"
+    project_ids = json.loads(project_ids_str)
+    
+    for project_id in project_ids:
+        query=f"""
+            SELECT * FROM project 
+            WHERE id = {project_id}
+        """
+        data_df = pd.read_sql(query, engine)
+        company_name = data_df["construction_company"].values[0]
+        break
 
+    for project_id in project_ids:
+        project_ids_str = ", ".join(str(project_id) for project_id in project_ids)
+    
     project_name_query = f"""
     SELECT project_name
     FROM project
-    WHERE construction_company = '{company_name}'
-    LIMIT 4
+    WHERE construction_company = "{company_name}"
+    AND id IN ({project_ids_str});
     """
+    
     project_names = pd.read_sql_query(project_name_query, engine)["project_name"]
-
+    
     # 각 프로젝트에 대해 concrete_df 생성
     concrete_dfs = [
         generate_concrete_df(engine, project_name) for project_name in project_names
     ]
 
-    # 2x2 subplot 생성
-    fig = make_subplots(rows=2, cols=2, subplot_titles=project_names)  # 서브플롯에 제목 추가
+    num_projects = len(project_ids)
+    num_rows = math.ceil(math.sqrt(num_projects))
+    num_cols = math.ceil(num_projects / num_rows)
+    
+    # subplot 생성
+    fig = make_subplots(rows=num_rows, cols=num_cols, subplot_titles=project_names)  # 서브플롯에 제목 추가
 
     # 각 프로젝트별로 그래프 추가
     for i, df in enumerate(concrete_dfs, start=1):
+        row = (i - 1) // num_cols + 1
+        col = (i - 1) % num_cols + 1
         fig.add_trace(
             go.Bar(
                 x=df["material_name"],
                 y=df["total_concrete_volume"],
                 showlegend=False,  # 레전드를 표시하지 않습니다
             ),
-            row=(i - 1) // 2 + 1,
-            col=(i - 1) % 2 + 1,
+            row=row,
+            col=col,
         )  # 각 subplot 위치 지정 (2x2 배열)
 
     fig.update_layout(
         title=f"{company_name}의 프로젝트별 콘크리트 구분에 따른 볼륨 분포 비교",
         title_font=dict(size=20, color="black"),
         xaxis_title="콘크리트 구분",
-        yaxis_title="콘크리트 볼륨 (m^3)",
+        yaxis_title="콘크리트 볼륨 (㎥)",
         width=900,
-        height=800,  # 적절히 높이 조절
+        height=400 * num_rows,  # 적절히 높이 조절
     )
 
     fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    explanation = f"""이 그래프는 {company_name}의 네 개 프로젝트에 대한 콘크리트 종류별 사용량 분포를 보여줍니다. 
+    explanation = f"""이 그래프는 {company_name}의 {num_projects} 개 프로젝트에 대한 콘크리트 종류별 사용량 분포를 보여줍니다. 
     각 서브 그래프는 하나의 프로젝트를 나타내며, 서브 그래프의 제목은 해당 프로젝트의 이름입니다.
     각 서브 그래프에서 x축은 콘크리트의 종류, y축은 해당 콘크리트 종류의 볼륨(㎥)을 나타냅니다. 
     이를 통해 프로젝트마다 사용하는 콘크리트의 종류와 그 양을 비교하고 분석할 수 있습니다.
@@ -486,9 +572,9 @@ def get_insight_4():
         color="construction_company",  # 바의 색상을 건설사에 따라 다르게 지정
         labels={
             "construction_company": "건설사",
-            "rebar_weight_per_concrete_volume": "콘크리트당 철근중량 (ton/m^3)",
+            "rebar_weight_per_concrete_volume": "콘크리트당 철근중량 (ton/㎥)",
         },
-        title="건설사 별 콘크리트당 철근중량 (ton/m^3)",
+        title="건설사 별 콘크리트당 철근중량 (ton/㎥)",
     )
 
     fig.update_layout(
@@ -616,7 +702,7 @@ def get_insight_5():
         title="'우미건설' 프로젝트에서 내력벽의 그루핑에 따른 콘크리트당 철근값의 비교",
         labels={
             "group_name": "내력벽 그룹명",
-            "rebar_weight_per_concrete": "콘크리트당 철근량 (ton/m^3)",
+            "rebar_weight_per_concrete": "콘크리트당 철근량 (ton/㎥)",
         },
         color="group_name",  # 여기에서 Bar의 색을 group_name에 따라 다르게 지정
     )
@@ -819,108 +905,4 @@ def get_insight_6():
     # Convert the list to json
     figures_json = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return figures_json
-
-
-
-@router.get("/insight/customed/{construction_company}")
-def get_insight_customed(construction_company: str, project_name: str):
-    # 예제 1번
-    # company_name의 모든 프로젝트에 있는 빌딩별, 콘크리트 M3당 철근량(ton) 값에 대한 분석
-    # 그래프 1 : 빌딩별 콘크리트 M3당 철근량(ton) 값의 분포 히스토그램
-    # 그래프 2 : 프로젝트별 콘크리트 M3당 철근량(ton) 값의 평균값 비교
-
-    # 계산 로직
-    df = get_materials_data_by_company(construction_company, 5)
-    # 그래프1
-    # make plotly histogram chart
-    # Create a histogram
-    data = [go.Histogram(x=df["rebar_per_conc"], nbinsx=30, marker=dict(color="navy"))]
-
-    # Create a layout
-    layout = go.Layout(
-        title_text=f"""{construction_company}, 빌딩별 콘크리트 볼륨(m^3)당 철근량(ton)의 분포표""",
-        title_font=dict(size=24, color="black"),
-        xaxis_title="콘크리트 볼륨(m^3)당 철근량(ton)",
-        yaxis_title="빈도(빌딩수)",
-        bargap=0.1,  # gap between bars of adjacent location coordinates
-    )
-
-    # Create a fig from data and layout, and plot the fig
-    fig = go.Figure(data=data, layout=layout)
-
-    # Save the plot to HTML file
-    # py.plot(fig, filename='histogram_plotly.html')
-    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    explanation1 = f"""이 그래프는 DB에 저장되어 있는 {construction_company}이 실행한 모든 프로젝트에서의 빌딩 별 콘크리트 볼륨당 철근 사용량(ton)을 표시한 히스토그램입니다. 
-    히스토그램은 데이터의 분포와 패턴을 시각화하는 데 사용되는 통계 그래프입니다. 이 경우, x축은 콘크리트 볼륨 당 철근의 양을 나타내고, y축은 해당 철근 사용량이 발생한 빌딩의 수, 즉 빈도를 나타냅니다. 
-    각각의 막대는 콘크리트 볼륨당 철근의 양이 얼마나 자주 발생하는지를 나타내는 빈도를 표시하고 있습니다. 
-    따라서, 이 히스토그램을 통해 우미건설의 프로젝트들에서 철근 사용량의 일반적인 분포와 트렌드를 쉽게 확인할 수 있습니다. 
-    이 정보는 프로젝트의 자원 계획과 예산 설정에 중요한 역할을 할 수 있습니다."""
-    fig_json_with_explanation1 = add_explanation(fig_json, explanation1)
-
-    # 그래프2
-
-    # Calculate the average rebar_per_conc grouped by project_name
-    rebar_per_conc_avg = (
-        df.groupby("project_name")["rebar_per_conc"].mean().reset_index()
-    )
-    rebar_per_conc_avg["project_name_truncated"] = rebar_per_conc_avg[
-        "project_name"
-    ].str[:20]
-
-    # Create a bar chart with different colors
-    data = [
-        go.Bar(
-            x=rebar_per_conc_avg["project_name_truncated"],
-            y=rebar_per_conc_avg["rebar_per_conc"],
-            marker=dict(
-                color=random_color(len(rebar_per_conc_avg["project_name_truncated"]))
-            ),
-        )
-    ]
-
-    # Create a layout
-    layout = go.Layout(
-        title=f"""{construction_company}, 프로젝트별 빌딩의 콘크리트 볼륨(m^3)당 철근량(ton)의 평균값""",
-        title_font=dict(size=24, color="black"),
-        xaxis=dict(title="프로젝트명"),
-        yaxis=dict(title="콘크리트 볼륨(m^3)당 철근량(ton)의 평균값"),
-        autosize=False,
-        width=1000,
-        height=600,
-        margin=go.layout.Margin(
-            l=50,
-            r=50,
-            b=100,  # Adjust this value to change the space for x-axis labels
-            t=100,
-            pad=4,
-        ),
-    )
-
-    # Create a fig from data and layout, and plot the fig
-    fig2 = go.Figure(data=data, layout=layout)
-
-    # Save the plot to HTML file
-    # py.plot(fig, filename='average_rebar_per_conc_plotly.html')
-    # Convert the figure to JSON
-    fig_json2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-
-    explanation2 = f"""이 그래프는 DB에 저장된 {construction_company}의 모든 프로젝트에 대한 빌딩별 콘크리트 볼륨(m³)당 철근 사용량(ton)의 평균값을 비교하고 있습니다.
-    각 프로젝트에 대해 계산된 평균 철근 사용량이 표시되므로, 프로젝트 간에 철근 사용량의 차이를 쉽게 비교할 수 있습니다. 
-    이 정보는 우미건설의 프로젝트 관리자가 프로젝트 간 자원 사용의 효율성을 비교하고, 더 효율적인 구조물 건설에 필요한 조치를 결정하는 데 도움이 될 수 있습니다. 
-    이 비교를 통해, 어떤 프로젝트에서는 철근을 더 많이, 또는 더 적게 사용해야 하는지에 대한 통찰력을 얻을 수 있습니다. 
-    이는 공사비용을 줄이고, 전체적인 프로젝트 실행을 개선하는 데 중요한 첫걸음이 될 수 있습니다."""
-    fig_json_with_explanation2 = add_explanation(fig_json2, explanation2)
-
-    # Load the jsons as Python dictionaries
-    fig_dict1 = json.loads(fig_json_with_explanation1)
-    fig_dict2 = json.loads(fig_json_with_explanation2)
-
-    # Create a new list that contains both figures
-    figures = [fig_dict1, fig_dict2]
-
-    # Convert the list to json
-    figures_json = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
     return figures_json
