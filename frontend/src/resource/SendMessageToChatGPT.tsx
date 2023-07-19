0c0d820f1fc897d648a3fda17d1f9e9876834024
unknown <cksdyd8350@gmail.com>
2023-07-19 12:38:13 +0900

    a

--- frontend/src/resource/SendMessageToChatGPT.tsx
+++ frontend/src/resource/SendMessageToChatGPT.tsx
@@ -4,21 +4,30 @@ import chatGPT from "./chatGPT_api.json";
 const SendMessageToChatGPT = async (messageText: string): Promise<string> => {
  
   try {
-    console.log(`Bearer ${chatGPT.API_key}`);
+    //console.log(`Bearer ${chatGPT.API_key}`);
 
-    const response = await axios.post(
+    console.log("??")
+    //message: messageText,
+    const data = JSON.stringify({
+      "model":"gpt-3.5-turbo",
+      "messages":[
+        {"role":"system", "content":""},
+        {"role":"user", "content":messageText}
+      ]
+    })
+    const response = await fetch(
       "https://api.openai.com/v1/chat/completions",
       {
-        message: messageText,
+      method:"POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + chatGPT.API_key,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${chatGPT.API_key}`,
-        },
-      }
+      body: data,
+    }
     );
 
+    console.log(response)
     // console.log(response.data.choices[0].message);
     // return response.data;
     return "hello";
