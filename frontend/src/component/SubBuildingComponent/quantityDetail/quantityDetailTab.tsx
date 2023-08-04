4a74226b12277e7e4a0a5f491bb33f737fd8cc20
unknown <cksdyd8350@gmail.com>
2023-08-04 16:53:48 +0900

    Fixed quantityDetail

--- frontend/src/component/SubBuildingComponent/quantityDetail/quantityDetailTab.tsx
+++ frontend/src/component/SubBuildingComponent/quantityDetail/quantityDetailTab.tsx
@@ -5,6 +5,7 @@ import {
 } from "@progress/kendo-react-inputs";
 import SingleColTable from "./SingleColTable";
 import QuantityFilter from "./quantityFilter";
+import PivotTable from "./pivotTable";
 import "./../../../styles/analysisTab.scss";
 
 const QuantityDetailTab = (props: any) => {
@@ -20,42 +21,47 @@ const QuantityDetailTab = (props: any) => {
 
   return (
     <div>
-      <QuantityFilter
-        setGridData={setGridData}
-        buildingInfo={props.buildingInfo}
-        selectedType={selectedType}
-      ></QuantityFilter>
+      <div className="filter-container">
+        <div>
+          <QuantityFilter
+            setGridData={setGridData}
+            buildingInfo={props.buildingInfo}
+            selectedType={selectedType}
+          ></QuantityFilter>
+        </div>
 
-      <div className="radio-button-container">
-        <RadioButton
-          value="concrete"
-          checked={selectedType === "concrete"}
-          label="콘크리트"
-          onChange={onTypeChange}
-          style={{ marginLeft: "10px" }}
-          className="k-radio-button"
-        />
-        <RadioButton
-          value="formwork"
-          checked={selectedType === "formwork"}
-          label="거푸집"
-          onChange={onTypeChange}
-          style={{ marginLeft: "10px" }}
-          className="k-radio-button"
-        />
-        <RadioButton
-          value="rebar"
-          checked={selectedType === "rebar"}
-          label="철근"
-          onChange={onTypeChange}
-          style={{ marginLeft: "10px" }}
-          className="k-radio-button"
-        />
+        <div className="radio-button-container">
+          <RadioButton
+            value="concrete"
+            checked={selectedType === "concrete"}
+            label="콘크리트"
+            onChange={onTypeChange}
+            style={{ marginLeft: "10px" }}
+            className="k-radio-button"
+          />
+          <RadioButton
+            value="formwork"
+            checked={selectedType === "formwork"}
+            label="거푸집"
+            onChange={onTypeChange}
+            style={{ marginLeft: "10px" }}
+            className="k-radio-button"
+          />
+          <RadioButton
+            value="rebar"
+            checked={selectedType === "rebar"}
+            label="철근"
+            onChange={onTypeChange}
+            style={{ marginLeft: "10px" }}
+            className="k-radio-button"
+          />
+        </div>
       </div>
 
       <div className="info-table-container" style={{ maxWidth: "90vw" }}>
         <SingleColTable data={gridData}></SingleColTable>
       </div>
+      <PivotTable data={gridData}></PivotTable>
     </div>
   );
 };
